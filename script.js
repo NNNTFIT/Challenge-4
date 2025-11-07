492a480c9ed451ed545ff0853094c714c10c061c
NNTFIT <nnt000fit@gmail.com>
2025-11-07 19:36:44 +0700

    :test5

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ img.forEach(img => {
 Close.addEventListener('click', () => {
   modal.style.display = 'none';
 });
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
