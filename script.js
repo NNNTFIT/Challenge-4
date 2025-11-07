f4e325119638ff565037423370f195c32b072fd1
NNTFIT <nnt000fit@gmail.com>
2025-11-07 19:06:14 +0700

    :test1

---
+++ script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.modal');
+const modalimg = document.getElementById('modal-img');
+const Close = document.querySelector('.close');
+const img = document.querySelectorAll('.container-img');
+img.forEach(img => {
+  img.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalimg.src = img.src;
+  });
+});
+Close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
